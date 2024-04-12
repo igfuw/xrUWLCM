3b7c1c3595f492124e5c44ce5a498e59f9dd7d91
Piotr Dziekan <pdziekanp@gmail.com>
2024-04-12 15:56:36 +0200

    load size spectrum + fixes

--- load.py
+++ load.py
@@ -1,11 +1,15 @@
 import xarray as xr
 from functools import partial
+import numpy as np
 
+# returns:
+# data - constants and all timesteps with DSD vars dropped
+# data_DSD - only timesteps that have DSD vars, from constants only rhod to facilitate calculations of derived variables
 def load_outdir(datadir):
     const = load_const(datadir)
-    data = xr.merge([const, load_timesteps(datadir, const)])
-    data = data.chunk({"t" : 1}) #for dask, each task executes takes one file
-    return data
+    data = xr.merge([const, load_timesteps(datadir, const)]).chunk({"t" : 1}) #set chunk for dask, each task executes takes one file
+    data_DSD = load_DSD(datadir, const).assign(rhod=const.rhod).chunk({"t" : 1}) #ditto
+    return data, data_DSD 
 
 def load_const(datadir):
     const = xr.open_dataset(datadir + "const.h5")
@@ -28,7 +32,7 @@ def load_const(datadir):
         try:
             const = const.merge(xr.open_dataset(datadir + "const.h5", group="/"+grname+"/"), combine_attrs="no_conflicts")
         except:
-            print("Group " + granme + " not found in " + datadir + "const.h5")
+            print("Group " + grname + " not found in " + datadir + "const.h5")
     return const
 
 
@@ -41,15 +45,36 @@ def load_timesteps(datadir, const):
             filenames.append(filename)
         except:
             continue
-    _squeeze_and_set_time = partial(squeeze_and_set_time, const=const)
+    _squeeze_and_set_time = partial(squeeze_and_set_time, const=const, drop_DSD=True)
+    print(filenames)
     return xr.open_mfdataset(filenames, parallel=False, preprocess=_squeeze_and_set_time)
 
-def squeeze_and_set_time(ds, const):
+
+#load size spectra
+def load_DSD(datadir, const):
+    filenames = []
+    for t in const.T.values:
+        filename=datadir + "timestep"+str(int(t / (const.dt))).zfill(10)+".h5"
+        try:
+            ds = xr.open_dataset(filename)
+            if 'rw_rng000_mom0' in ds.variables:
+                filenames.append(filename)
+        except:
+            continue
+    _squeeze_and_set_time = partial(squeeze_and_set_time, const=const, drop_DSD=False)
+    return xr.open_mfdataset(filenames, parallel=False, preprocess=_squeeze_and_set_time)
+    #_squeeze_and_set_time = partial(squeeze_and_set_time, const=const)
+    #return xr.open_mfdataset(filenames, parallel=False, preprocess=_squeeze_and_set_time)
+
+
+def squeeze_and_set_time(ds, const, drop_DSD):
     ds = ds.rename({"phony_dim_0" : "x", "phony_dim_1" : "y", "phony_dim_2" : "z"})
-    ds = ds.squeeze("phony_dim_3") # surface fluxes are 3D arrays with len(z)=1, convert to 2D arrays
+#    ds = ds.squeeze("phony_dim_3") # surface fluxes are 3D arrays with len(z)=1, convert to 2D arrays
     ds = ds.expand_dims("t")
     #get time from filename
-    t = int(ds.encoding["source"][-13:-3]) * const.dt 
+    t = np.float32(ds.encoding["source"][-13:-3]) * const.dt 
+    print(t, np.float32(ds.encoding["source"][-13:-3]))
+    
     ds = ds.assign_coords({"x" : const.x, "y" : const.y, "z" : const.z, "t" : [t]})#, "Y", "Z", "T"])
     #read puddle
     ds_puddle = xr.open_dataset(ds.encoding["source"], group="/puddle/")
@@ -57,4 +82,12 @@ def squeeze_and_set_time(ds, const):
     for name in ds_puddle.attrs:
         ds_puddle = ds_puddle.rename({name : "puddle_"+name}) # rename variables to indicate that this is puddle
     ds = ds.merge(ds_puddle) # ds_puddle attributes are dropped on merge (thats good)
+    
+    #drop size spectrum data, because it may not be available at all timesteps making merging difficult; to load the spectrum, use load_spectra
+    if(drop_DSD):
+        out_spec_names = []
+        for i in np.arange(30):
+            out_spec_names.append('rw_rng'+str(i).zfill(3)+'_mom0')
+        ds = ds.drop_vars(out_spec_names, errors='ignore')
+        
     return ds
